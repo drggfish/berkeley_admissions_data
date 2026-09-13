a6d902c6073d73f786df1684e4de22fe8e2c5c60
drggfish <drggfish@gmail.com>
2026-09-13 13:47:55 -0400

    Update bad_3.R
    Modified in Visual Studio

--- bad_3.R
+++ bad_3.R
@@ -10,8 +10,6 @@ ucbadmit_raw <- read.csv("ucb-admit.csv",header=TRUE)
 
 head(ucbadmit_raw)
 
-summary(ucb_data_full)
-
 # 1. Filter, select, and save to a new data frame
 ucbadmit <- ucbadmit_raw %>%
   rename(gender = Gender) %>%
